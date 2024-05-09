240700b20268520ad3028169a32893d3ba948565
Matt Hotstone <matt_hotstone@hotmail.com>
2024-05-09 11:12:24 +1000

    Initial Commit

---
+++ CaloriesPredictor/CaloriesPredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesPredictorApp.swift
+//  CaloriesPredictor
+//
+//  Created by Matt Hotstone on 9/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesPredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
